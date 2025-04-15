799d1525a87266c1f82c53294cc96f0332f142c5
mohammad asad <asadkhan41376@gmail.com>
2025-04-15 10:36:57 +0530

    add new activity C17-L1-AA

---
+++ src/app/C17-L1-AA/page.tsx
@@ -0,0 +1,12 @@
+import CarouselC17L1AA from '@/carouselC17-L1-AA'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <CarouselC17L1AA/>
+    </div>
+  )
+}
+
+export default Page
