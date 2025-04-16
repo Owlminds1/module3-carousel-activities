3c1e2b0a0a9cd810fcbce400bd328e64fbe3ba4a
mohammad asad <asadkhan41376@gmail.com>
2025-04-16 15:24:08 +0530

    some change in L2-A4

--- src/carouselC17-L2-A6/lunchBoxSlide.tsx
+++ src/carouselC17-L2-A6/lunchBoxSlide.tsx
@@ -19,27 +19,46 @@ const LunchBoxSlide = ({
   setFruitsCal,
   setCountCal,
 }: myProps) => {
+  const [selectedIFruite, setSelectedFruite] = useState<number[]>([]);
+  const [selectedVegges, setSelectedVegges] = useState<number[]>([]);
+  const [selectedMeals, sesMelectedMeals] = useState<number[]>([]);
 
-  const [selectedItem, setSelectedItem] = useState<number[]>([]);
-
-
-  const handleFruitsCalories = (calories: number,index:number) => {
+  const handleFruitsCalories = (calories: number, index: number) => {
     if (isNaN(calories)) return;
-    setFruitsCal((prev) => prev + calories);
-    setCountCal((prev) => prev + calories);
-    setSelectedItem((prev)=>[...prev,index])
+
+    if (!selectedIFruite.includes(index)) {
+      setFruitsCal((prev) => prev + calories);
+      setCountCal((prev) => prev + calories);
+      setSelectedFruite((prev) => [...prev, index]);
+    } else {
+      setFruitsCal((prev) => prev - calories);
+      setCountCal((prev) => prev - calories);
+      setSelectedFruite((prev) => prev.filter((item) => item !== index));
+    }
   };
-  const handleVaggeisCalories = (calories: number,index:number) => {
+  const handleVaggeisCalories = (calories: number, index: number) => {
     if (isNaN(calories)) return;
-    setVaggeisCal((prev) => prev + calories);
-    setCountCal((prev) => prev + calories);
-    setSelectedItem((prev)=>[...prev,index])
+    if (!selectedVegges.includes(index)) {
+      setVaggeisCal((prev) => prev + calories);
+      setCountCal((prev) => prev + calories);
+      setSelectedVegges((prev) => [...prev, index]);
+    } else {
+      setVaggeisCal((prev) => prev - calories);
+      setCountCal((prev) => prev - calories);
+      setSelectedVegges((prev) => prev.filter((item) => item !== index));
+    }
   };
-  const handleMealCalories = (calories: number,index:number) => {
+  const handleMealCalories = (calories: number, index: number) => {
     if (isNaN(calories)) return;
-    setMealCal((prev) => prev + calories);
-    setCountCal((prev) => prev + calories);
-    setSelectedItem((prev)=>[...prev,index])
+    if (!selectedMeals.includes(index)) {
+      setMealCal((prev) => prev + calories);
+      setCountCal((prev) => prev + calories);
+      sesMelectedMeals((prev) => [...prev, index]);
+    } else {
+      setMealCal((prev) => prev - calories);
+      setCountCal((prev) => prev - calories);
+      sesMelectedMeals((prev) => prev.filter((item) => item !== index));
+    }
   };
 
   return (
@@ -51,63 +70,77 @@ const LunchBoxSlide = ({
         <div className="col-span-6 flex flex-col gap-3">
           {/* ====================fruits data ===================== */}
 
-         <div>
-          <h4 className="text-lg text-center text-black py-2">Fruits</h4>
-         <div className="border-2 grid grid-cols-12 p-2 gap-1 bg-violet-100 rounded-lg min-h-[200px]">
-            
-           
-            {fruitsData.map((fitem, findex) => (
-              <div className="col-span-3" key={findex}>
-                <div
-                  className={ `${selectedItem.includes(findex) ? "border-2 border-black":""} w-[150px] h-[150px] bg-white relative rounded-lg overflow-hidden border cursor-pointer hover:border-2 hover:border-black`}
-                  onClick={() => handleFruitsCalories(fitem.value,findex)}
-                >
-                  <Image src={fitem.fruitsImg} fill alt="fruits imgs" />
+          <div>
+            <h4 className="text-lg text-center text-black py-2">Fruits</h4>
+            <div className="border-2 grid grid-cols-12 p-2 gap-1 bg-violet-100 rounded-lg min-h-[200px]">
+              {fruitsData.map((fitem, findex) => (
+                <div className={`col-span-3 ${
+                  selectedIFruite.includes(findex)
+                    ? "border-2  border-green-800 rounded-lg"
+                    : ""
+                } `} key={findex}>
+                  <div
+                    className={`w-[150px] h-[150px] bg-white relative rounded-lg overflow-hidden  cursor-pointer hover:border-1 hover:border-black`}
+                    onClick={() => handleFruitsCalories(fitem.value, findex)}
+                  >
+                    <Image src={fitem.fruitsImg} fill alt="fruits imgs" />
+                  </div>
+                  <h2 className="text-black  text-center text-lg py-2">
+                    {fitem.name}
+                  </h2>
                 </div>
-                <h2 className="text-black  text-center text-lg py-2">
-                  {fitem.name}
-                </h2>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
-         </div>
           {/* ====================veggies data ===================== */}
           <div>
-          <h4 className="text-lg text-center text-black py-2">Veggies</h4>
-          <div className="border-2 grid grid-cols-12 p-2 gap-1 bg-violet-100 rounded-lg min-h-[200px]">
-            {veggiesData.map((fitem, findex) => (
-              <div className="col-span-3" key={findex}>
-                <div
-                   className={ `${selectedItem.includes(findex) ? "border-2 border-black":""} w-[150px] h-[150px] bg-white relative rounded-lg overflow-hidden border cursor-pointer hover:border-2 hover:border-black`}
-                  onClick={() => handleVaggeisCalories(fitem.value,findex)}
-                >
-                  <Image src={fitem.fruitsImg} fill alt="fruits imgs" />
+            <h4 className="text-lg text-center text-black py-2">Veggies</h4>
+            <div className="border-2 grid grid-cols-12 p-2 gap-1 bg-violet-100 rounded-lg min-h-[200px]">
+              {veggiesData.map((fitem, Vindex) => (
+                <div  className={`col-span-3 ${
+                  selectedVegges.includes(Vindex)
+                    ? "border-2  border-green-800 rounded-lg"
+                    : ""
+                } `} key={Vindex}>
+                  <div
+                    className={`w-[150px] h-[150px] bg-white relative rounded-lg overflow-hidden cursor-pointer hover:border-1 hover:border-black`}
+                    onClick={() => handleVaggeisCalories(fitem.value, Vindex)}
+                  >
+                    <Image src={fitem.fruitsImg} fill alt="fruits imgs" />
+                  </div>
+                  <h2 className="text-black  text-center text-lg py-2">
+                    {fitem.name}
+                  </h2>
                 </div>
-                <h2 className="text-black  text-center text-lg py-2">
-                  {fitem.name}
-                </h2>
-              </div>
-            ))}
-          </div>
+              ))}
+            </div>
           </div>
           {/* ====================meals data ===================== */}
           <div>
-          <h4 className="text-lg text-center text-black py-2">Meals</h4>
-          <div className="border-2 grid grid-cols-12 p-2 gap-1 bg-violet-100 rounded-lg min-h-[200px]">
-            {MealsData.map((fitem, findex) => (
-              <div className="col-span-3" key={findex}>
+            <h4 className="text-lg text-center text-black py-2">Meals</h4>
+            <div className="border-2 grid grid-cols-12 p-2 gap-1 bg-violet-100 rounded-lg min-h-[200px]">
+              {MealsData.map((fitem, Mindex) => (
                 <div
-                 className={ `${selectedItem.includes(findex) ? "border-2 border-black":""} w-[150px] h-[150px] bg-white relative rounded-lg overflow-hidden border cursor-pointer hover:border-2 hover:border-black`}
-                  onClick={() => handleMealCalories(fitem.value,findex)}
+                  className={`col-span-3 ${
+                    selectedMeals.includes(Mindex)
+                      ? "border-2  border-green-800 rounded-lg"
+                      : ""
+                  } `}
+                  key={Mindex}
                 >
-                  <Image src={fitem.fruitsImg} fill alt="fruits imgs" />
+                  <div
+                    className={`
+                    w-[150px] h-[150px] bg-white relative rounded-lg overflow-hidden  cursor-pointer hover:border-1 hover:border-black`}
+                    onClick={() => handleMealCalories(fitem.value, Mindex)}
+                  >
+                    <Image src={fitem.fruitsImg} fill alt="fruits imgs" />
+                  </div>
+                  <h2 className="text-black  text-center text-lg py-2">
+                    {fitem.name}
+                  </h2>
                 </div>
-                <h2 className="text-black  text-center text-lg py-2">
-                  {fitem.name}
-                </h2>
-              </div>
-            ))}
-          </div>
+              ))}
+            </div>
           </div>
         </div>
 
@@ -121,9 +154,12 @@ const LunchBoxSlide = ({
           </h3>
 
           <button
-            onClick={() => countCal >= 700? setIsFirstScreen("table"):""
-            }
-            className="bg-yellow-400 cursor-pointer text-black px-5 py-2 rounded-lg" 
+            onClick={() => (countCal >= 700 ? setIsFirstScreen("table") : "")}
+            className={`${
+              countCal >= 700
+                ? "bg-yellow-400 cursor-pointer"
+                : "bg-yellow-200 cursor-no-drop"
+            }   text-black px-5 py-2 rounded-lg`}
           >
             Check Your Calories
           </button>
