aac83e74edcd1dd8ff1d066fe692d234bb152402
mohammad asad <asadkhan41376@gmail.com>
2025-04-18 11:38:44 +0530

    add two new activity C18-L1-A4,A5

---
+++ src/app/C18-L1-A5/page.tsx
@@ -0,0 +1,12 @@
+import CarouselC18L1A5 from '@/carouselC18-L1-A5'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <CarouselC18L1A5/>
+    </div>
+  )
+}
+
+export default Page
---
+++ src/carouselC18-L1-A5/index.tsx
@@ -0,0 +1,42 @@
+"use client";
+import React, { useState } from "react";
+import Strart from "./start";
+import Table from "./table";
+import ShopingCart from "./shopingCart";
+
+const CarouselC18L1A5 = () => {
+  const [isFirstScreen, setIsFirstScreen] = useState("start");
+  const [fruitsCal, setFruitsCal] = useState(0);
+  const [vaggeisCal, setVaggeisCal] = useState(0);
+  const [mealCal, setMealCal] = useState(0);
+
+  const [countCal, setCountCal] = useState(0);
+
+  return (
+    <>
+      {isFirstScreen == "start" && (
+        <Strart setIsFirstScreen={setIsFirstScreen} />
+      )}
+      {isFirstScreen == "CartSlide" && (
+        <ShopingCart
+          setFruitsCal={setFruitsCal}
+          setVaggeisCal={setVaggeisCal}
+          setMealCal={setMealCal}
+          countCal={countCal}
+          setCountCal={setCountCal}
+          setIsFirstScreen={setIsFirstScreen}
+        />
+      )}
+      {isFirstScreen == "table" && (
+        <Table
+          fruitsCal={fruitsCal}
+          vaggeisCal={vaggeisCal}
+          mealCal={mealCal}
+          countCal={countCal}
+        />
+      )}
+    </>
+  );
+};
+
+export default CarouselC18L1A5;
