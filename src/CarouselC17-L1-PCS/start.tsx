676b2cf1e301628923e2bef369d955b7cef9e16c
mohammad asad <asadkhan41376@gmail.com>
2025-06-17 12:44:13 +0530

    some chnage in c17-l1-pcs

--- src/CarouselC17-L1-PCS/start.tsx
+++ src/CarouselC17-L1-PCS/start.tsx
@@ -7,7 +7,7 @@ const Start = ({setIsFirstScreen}:myProps) => {
   return (
     <div className="min-h-screen flex justify-center items-center flex-col gap-10 bg-[#F8FAFC]">
     <div className="min-h-[100px]  shadow-black  p-4 shadow-md rounded-lg flex text-center justify-center items-center w-[600px]">
-    <h2 className="text-2xl text-black">Circle the different object</h2>
+    <h2 className="text-2xl text-black">Circle the Different Object</h2>
     </div>
   <StartBtn value='Start' setIsFirstScreen={setIsFirstScreen} screenVal="Qustions" />
   </div>
